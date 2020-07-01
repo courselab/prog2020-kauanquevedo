1c8856b1f250cb599bad51c6bd27ac5f8ccc3480
kauanquevedo <kauan.quevedo@usp.br>
2020-07-01 03:37:44 +0000

    correção m008

--- m008.c
+++ m008.c
@@ -71,13 +71,13 @@ void lastname (char *s)
 
     }         
   }
-  /*A variável q determina o tamanho do sobrenome e depois coloco a ',' e o espaço na string s.*/
+  /*A variável 'q' determina o tamanho do sobrenome e depois coloco a ',' e o espaço na string s.*/
 
   q = (TAM1 - 1 - count1 -1);
   (s[q]) = ',';
   (s[q + 1]) = ' ';
   
-  
+  /*Determino a posição do primeiro espaço na nova string s*/
   for (m = 0; m <= MAX; m++)         
     { 
         n = (s[m]);
@@ -85,18 +85,18 @@ void lastname (char *s)
           count2 = m;
         break;}
     }  
+  /* Finalizo a string s logo depois do primeiro espaço e finalizo a string t na posição do último espaço, em seguida concateno a string t (que vai do primeiro nome até o espaço anterior ao sobrenome) na string s.*/
+
   s[count2 + 1] = '\0';  
-  t[count1 + 1] = '\0';
-  strcat (s, t); 
+  t[count1] = '\0';
+  strcat (s, t);
 
   /*for(o = (count2 + 1); o < count1 ; o++)
   {
-    s[o] = t[p] ;
-    
+    s[o] = (t[p]) ;
     p++;
-  }
-  TAM2 = strlen(s);
-  s[TAM2 + 1] = '\0';*/
+  }*/
+  
 
 }
 
